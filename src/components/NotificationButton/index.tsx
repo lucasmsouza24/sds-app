04cd8fb45e6918b10376d9ec1c5349824c7a494f
lucasmsouza24 <lucasmesquita58@hotmail.com>
2022-09-11 04:57:28 -0300

    toastify

--- src/components/NotificationButton/index.tsx
+++ src/components/NotificationButton/index.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 import icon from "../../assets/img/notification-icon.svg";
 import Api from "../../Api";
+import { toast } from 'react-toastify';
 
 type Props = {
     saleId: number;
@@ -9,7 +10,12 @@ type Props = {
 function NotificationButton({saleId} : Props) {
 
     function handleClick(id: number) {
-        Api.post(`/sales/${id}/notification`);
+        Api.post(`/sales/${id}/notification`)
+        .then(({status}) => {
+            if (status == 201) {
+                toast("SMS enviado")
+            }
+        });
     }
 
     return(
